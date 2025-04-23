464a1bb35da863967dfca0f77559c63610267480
lgnbhl <felix.luginbuhl@protonmail.ch>
2025-04-23 11:20:20 +0200

    add basic Stepper example

---
+++ inst/examples/Stepper.R
@@ -0,0 +1,62 @@
+library(shinyMaterialUI)
+library(shiny)
+
+# https://mui.com/material-ui/react-stepper/#horizontal-stepper
+ui_Stepper <- Box(
+  sx = list(width = '100%'),
+  Box(
+    uiOutput(outputId = "basicStepper")
+  ),
+  Box(
+    sx = list(display = "flex", flexDirection = 'row', pt = 2),
+    Button.shinyInput(inputId = "backStep", "Back"),
+    Button.shinyInput(inputId = "nextStep", "Next"),
+    Button.shinyInput(inputId = "reset", "Reset")
+  )
+)
+
+server_Stepper <- function(input, output, session) {
+  stepIndex <- reactiveValues(value = 0)
+
+  # 'Next' button not disabled if value is 3
+  observe({
+    if(stepIndex$value != 3) updateButton.shinyInput(inputId = "nextStep", disabled = FALSE)
+  })
+  # 'Next' button disabled if value is 3
+  observeEvent(input$nextStep, {
+    stepIndex$value <- stepIndex$value + 1
+    if(stepIndex$value == 3) updateButton.shinyInput(inputId = "nextStep", disabled = TRUE)
+  })
+  observeEvent(input$backStep, {
+    stepIndex$value <- stepIndex$value - 1
+    if(stepIndex$value <= 0) stepIndex$value <- 0
+  })
+  observeEvent(input$reset, {
+    stepIndex$value <- 0
+    updateButton.shinyInput(inputId = "nextStep", disabled = FALSE)
+  })
+  
+  output$basicStepper <- renderUI({
+    Stepper(
+      activeStep = stepIndex$value,
+      #alternativeLabel = TRUE,
+      #nonLinear = TRUE,
+      Step(
+        StepLabel("Step 1"),
+        StepContent("Content step 1")
+      ),
+      Step(
+        StepLabel("Step 2"),
+        StepContent("Content step 2")
+      ),
+      Step(
+        StepLabel("Step 3"),
+        StepContent("Content step 3")
+      )
+    )
+  })
+}
+
+if (interactive()) {
+  shinyApp(ui = ui_Stepper, server = server_Stepper)
+}
--- inst/examples/showcase/app.R
+++ inst/examples/showcase/app.R
@@ -71,6 +71,7 @@ Sidebar <- TabContext.shinyInput(
       Tab(label = "Menu", value = "Menu"),
       Tab(label = "Tabs", value = "Tabs"),
       Tab(label = "Slider", value = "Slider"),
+      Tab(label = "Stepper", value = "Stepper"),
       Tab(label = "TextField", value = "TextField"),
       Tab(label = "ThemeProvider", value = "ThemeProvider")
     ),
@@ -83,6 +84,7 @@ Sidebar <- TabContext.shinyInput(
     TabPanel.shinyInput(inputId = "tabMenu", value = "Menu", create_TabPanel("Menu")),
     TabPanel.shinyInput(inputId = "tabTabs", value = "Tabs", create_TabPanel("Tabs")),
     TabPanel.shinyInput(inputId = "tabSlider", value = "Slider", create_TabPanel("Slider")),
+    TabPanel.shinyInput(inputId = "tabStepper", value = "Stepper", create_TabPanel("Stepper")),
     TabPanel.shinyInput(inputId = "tabTextField", value = "TextField", create_TabPanel("TextField")),
     TabPanel.shinyInput(inputId = "tabThemeProvider", value = "ThemeProvider", create_TabPanel("ThemeProvider"))
   )
@@ -142,6 +144,46 @@ server <- function(input, output) {
   # Slider
   output$SliderValue1 <- renderText({
     paste(input$DiscreteSliderLabel)
+  })
+  # Stepper
+  stepIndex <- reactiveValues(value = 0)
+  
+  ## 'Next' button not disabled if value is 3
+  observe({
+    if(stepIndex$value != 3) updateButton.shinyInput(inputId = "nextStep", disabled = FALSE)
+  })
+  ## 'Next' button disabled if value is 3
+  observeEvent(input$nextStep, {
+    stepIndex$value <- stepIndex$value + 1
+    if(stepIndex$value == 3) updateButton.shinyInput(inputId = "nextStep", disabled = TRUE)
+  })
+  observeEvent(input$backStep, {
+    stepIndex$value <- stepIndex$value - 1
+    if(stepIndex$value <= 0) stepIndex$value <- 0
+  })
+  observeEvent(input$reset, {
+    stepIndex$value <- 0
+    updateButton.shinyInput(inputId = "nextStep", disabled = FALSE)
+  })
+  
+  output$basicStepper <- renderUI({
+    Stepper(
+      activeStep = stepIndex$value,
+      #alternativeLabel = TRUE,
+      #nonLinear = TRUE,
+      Step(
+        StepLabel("Step 1"),
+        StepContent("Content step 1")
+      ),
+      Step(
+        StepLabel("Step 2"),
+        StepContent("Content step 2")
+      ),
+      Step(
+        StepLabel("Step 3"),
+        StepContent("Content step 3")
+      )
+    )
   })
   # TextField
   output$BasicTextFieldsValue1 <- renderText({
