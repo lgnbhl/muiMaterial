5a0e2e02cb2e32498cf71bd3bc74ba0ed4185f49
lgnbhl <felix.luginbuhl@protonmail.ch>
2025-05-06 15:46:14 +0200

    remove PrettoSlider example from Slider example

--- inst/examples/Slider.R
+++ inst/examples/Slider.R
@@ -15,71 +15,10 @@ DiscreteSliderLabel <- Box(
   verbatimTextOutput("SliderValue1")
 )
 
-# https://mui.com/material-ui/react-slider/#customization
-PrettoSlider <- ThemeProvider(
-  theme = list(
-    components = list(
-      MuiSlider = list(
-        styleOverrides = list(
-          root = list(
-            color = '#52af77',
-            height = 8,
-            '& .MuiSlider-track' = list(
-              border = 'none'
-            ),
-            '& .MuiSlider-thumb' = list(
-              height = 24,
-              width = 24,
-              backgroundColor = '#fff',
-              border = '2px solid currentColor',
-              '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible' = list(
-                boxShadow = 'inherit'
-              ),
-              '&::before' = list(
-                display = 'none'
-              )
-            ),
-            '& .MuiSlider-valueLabel' = list(
-              lineHeight = 1.2,
-              fontSize = 12,
-              background = 'unset',
-              padding = 0,
-              width = 32,
-              height = 32,
-              borderRadius = '50% 50% 50% 0',
-              backgroundColor = '#52af77',
-              transformOrigin = 'bottom left',
-              transform = 'translate(50%, -100%) rotate(-45deg) scale(0)',
-              '&::before' = list( display = 'none' ),
-              '&.MuiSlider-valueLabelOpen' = list(
-                transform = 'translate(50%, -100%) rotate(-45deg) scale(1)'
-              ),
-              '& > *' = list(
-                transform = 'rotate(45deg)'
-              )
-            )
-          )
-        )
-      )
-    )
-  ),
-  CssBaseline(
-    Box(
-      Slider.shinyInput(
-        inputId = "DiscreteSliderLabel",
-        value = 20,
-        valueLabelDisplay = "auto",
-        'aria-label' = "pretto slider"
-      )
-    )
-  )
-)
-
 ui_Slider <- CssBaseline(
   Stack(
     sx = list(flexDirection = 'row', p = 6, gap = "500px"),
-    DiscreteSliderLabel,
-    PrettoSlider
+    DiscreteSliderLabel
   )
 )
 
