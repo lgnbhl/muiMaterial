5c7c9ca6e2d204c6457de92aec5d3420ea5924b2
lgnbhl <felix.luginbuhl@protonmail.ch>
2026-02-19 21:05:15 +0100

    fix: :bug: add missing bracket and missing Tabs()

--- R/components.R
+++ R/components.R
@@ -4,579 +4,584 @@ component <- function(name, module = '@mui/material') {
   function(...) {
     shiny.react::reactElement(
       module = module,
-    name = name,
-    props = shiny.react::asProps(...),
-    deps = muiMaterialDependency()
-  )
+      name = name,
+      props = shiny.react::asProps(...),
+      deps = muiMaterialDependency()
+    )
+  }
 }
- 
+
 #' @rdname Accordion
 #' @export
 Accordion <- component('Accordion')
- 
+
 #' @rdname AccordionActions
 #' @export
 AccordionActions <- component('AccordionActions')
- 
+
 #' @rdname AccordionDetails
 #' @export
 AccordionDetails <- component('AccordionDetails')
- 
+
 #' @rdname AccordionSummary
 #' @export
 AccordionSummary <- component('AccordionSummary')
- 
+
 #' @rdname Alert
 #' @export
 Alert <- component('Alert')
- 
+
 #' @rdname AlertTitle
 #' @export
 AlertTitle <- component('AlertTitle')
- 
+
 #' @rdname AppBar
 #' @export
 AppBar <- component('AppBar')
- 
+
 #' @rdname Autocomplete
 #' @export
 Autocomplete <- component('Autocomplete')
- 
+
 #' @rdname Avatar
 #' @export
 Avatar <- component('Avatar')
- 
+
 #' @rdname AvatarGroup
 #' @export
 AvatarGroup <- component('AvatarGroup')
- 
+
 #' @rdname Backdrop
 #' @export
 Backdrop <- component('Backdrop')
- 
+
 #' @rdname Badge
 #' @export
 Badge <- component('Badge')
- 
+
 #' @rdname BottomNavigation
 #' @export
 BottomNavigation <- component('BottomNavigation')
- 
+
 #' @rdname BottomNavigationAction
 #' @export
 BottomNavigationAction <- component('BottomNavigationAction')
- 
+
 #' @rdname Box
 #' @export
 Box <- component('Box')
- 
+
 #' @rdname Breadcrumbs
 #' @export
 Breadcrumbs <- component('Breadcrumbs')
- 
+
 #' @rdname Button
 #' @export
 Button <- component('Button')
- 
+
 #' @rdname ButtonBase
 #' @export
 ButtonBase <- component('ButtonBase')
- 
+
 #' @rdname ButtonGroup
 #' @export
 ButtonGroup <- component('ButtonGroup')
- 
+
 #' @rdname Card
 #' @export
 Card <- component('Card')
- 
+
 #' @rdname CardActionArea
 #' @export
 CardActionArea <- component('CardActionArea')
- 
+
 #' @rdname CardActions
 #' @export
 CardActions <- component('CardActions')
- 
+
 #' @rdname CardContent
 #' @export
 CardContent <- component('CardContent')
- 
+
 #' @rdname CardHeader
 #' @export
 CardHeader <- component('CardHeader')
- 
+
 #' @rdname CardMedia
 #' @export
 CardMedia <- component('CardMedia')
- 
+
 #' @rdname Checkbox
 #' @export
 Checkbox <- component('Checkbox')
- 
+
 #' @rdname Chip
 #' @export
 Chip <- component('Chip')
- 
+
 #' @rdname CircularProgress
 #' @export
 CircularProgress <- component('CircularProgress')
- 
+
 #' @rdname ClickAwayListener
 #' @export
 ClickAwayListener <- component('ClickAwayListener')
- 
+
 #' @rdname Collapse
 #' @export
 Collapse <- component('Collapse')
- 
+
 #' @rdname Container
 #' @export
 Container <- component('Container')
- 
+
 #' @rdname CssBaseline
 #' @export
 CssBaseline <- component('CssBaseline')
- 
+
 #' @rdname Dialog
 #' @export
 Dialog <- component('Dialog')
- 
+
 #' @rdname DialogActions
 #' @export
 DialogActions <- component('DialogActions')
- 
+
 #' @rdname DialogContent
 #' @export
 DialogContent <- component('DialogContent')
- 
+
 #' @rdname DialogContentText
 #' @export
 DialogContentText <- component('DialogContentText')
- 
+
 #' @rdname DialogTitle
 #' @export
 DialogTitle <- component('DialogTitle')
- 
+
 #' @rdname Divider
 #' @export
 Divider <- component('Divider')
- 
+
 #' @rdname Drawer
 #' @export
 Drawer <- component('Drawer')
- 
+
 #' @rdname Fab
 #' @export
 Fab <- component('Fab')
- 
+
 #' @rdname Fade
 #' @export
 Fade <- component('Fade')
- 
+
 #' @rdname FilledInput
 #' @export
 FilledInput <- component('FilledInput')
- 
+
 #' @rdname FormControl
 #' @export
 FormControl <- component('FormControl')
- 
+
 #' @rdname FormControlLabel
 #' @export
 FormControlLabel <- component('FormControlLabel')
- 
+
 #' @rdname FormGroup
 #' @export
 FormGroup <- component('FormGroup')
- 
+
 #' @rdname FormHelperText
 #' @export
 FormHelperText <- component('FormHelperText')
- 
+
 #' @rdname FormLabel
 #' @export
 FormLabel <- component('FormLabel')
- 
+
 #' @rdname GlobalStyles
 #' @export
 GlobalStyles <- component('GlobalStyles')
- 
+
 #' @rdname Grid
 #' @export
 Grid <- component('Grid')
- 
+
 #' @rdname GridLegacy
 #' @export
 GridLegacy <- component('GridLegacy')
- 
+
 #' @rdname Grow
 #' @export
 Grow <- component('Grow')
- 
+
 #' @rdname Icon
 #' @export
 Icon <- component('Icon')
- 
+
 #' @rdname IconButton
 #' @export
 IconButton <- component('IconButton')
- 
+
 #' @rdname ImageList
 #' @export
 ImageList <- component('ImageList')
- 
+
 #' @rdname ImageListItem
 #' @export
 ImageListItem <- component('ImageListItem')
- 
+
 #' @rdname ImageListItemBar
 #' @export
 ImageListItemBar <- component('ImageListItemBar')
- 
+
 #' @rdname Input
 #' @export
 Input <- component('Input')
- 
+
 #' @rdname InputAdornment
 #' @export
 InputAdornment <- component('InputAdornment')
- 
+
 #' @rdname InputBase
 #' @export
 InputBase <- component('InputBase')
- 
+
 #' @rdname InputLabel
 #' @export
 InputLabel <- component('InputLabel')
- 
+
 #' @rdname LinearProgress
 #' @export
 LinearProgress <- component('LinearProgress')
- 
+
 #' @rdname Link
 #' @export
 Link <- component('Link')
- 
+
 #' @rdname List
 #' @export
 List <- component('List')
- 
+
 #' @rdname ListItem
 #' @export
 ListItem <- component('ListItem')
- 
+
 #' @rdname ListItemAvatar
 #' @export
 ListItemAvatar <- component('ListItemAvatar')
- 
+
 #' @rdname ListItemButton
 #' @export
 ListItemButton <- component('ListItemButton')
- 
+
 #' @rdname ListItemIcon
 #' @export
 ListItemIcon <- component('ListItemIcon')
- 
+
 #' @rdname ListItemSecondaryAction
 #' @export
 ListItemSecondaryAction <- component('ListItemSecondaryAction')
- 
+
 #' @rdname ListItemText
 #' @export
 ListItemText <- component('ListItemText')
- 
+
 #' @rdname ListSubheader
 #' @export
 ListSubheader <- component('ListSubheader')
- 
+
 #' @rdname Masonry
 #' @export
 Masonry <- component('Masonry')
- 
+
 #' @rdname Menu
 #' @export
 Menu <- component('Menu')
- 
+
 #' @rdname MenuItem
 #' @export
 MenuItem <- component('MenuItem')
- 
+
 #' @rdname MenuList
 #' @export
 MenuList <- component('MenuList')
- 
+
 #' @rdname MobileStepper
 #' @export
 MobileStepper <- component('MobileStepper')
- 
+
 #' @rdname Modal
 #' @export
 Modal <- component('Modal')
- 
+
 #' @rdname NativeSelect
 #' @export
 NativeSelect <- component('NativeSelect')
- 
+
 #' @rdname NoSsr
 #' @export
 NoSsr <- component('NoSsr')
- 
+
 #' @rdname OutlinedInput
 #' @export
 OutlinedInput <- component('OutlinedInput')
- 
+
 #' @rdname Pagination
 #' @export
 Pagination <- component('Pagination')
- 
+
 #' @rdname PaginationItem
 #' @export
 PaginationItem <- component('PaginationItem')
- 
+
 #' @rdname Paper
 #' @export
 Paper <- component('Paper')
- 
+
 #' @rdname PigmentContainer
 #' @export
 PigmentContainer <- component('PigmentContainer')
- 
+
 #' @rdname PigmentGrid
 #' @export
 PigmentGrid <- component('PigmentGrid')
- 
+
 #' @rdname PigmentStack
 #' @export
 PigmentStack <- component('PigmentStack')
- 
+
 #' @rdname Popover
 #' @export
 Popover <- component('Popover')
- 
+
 #' @rdname Popper
 #' @export
 Popper <- component('Popper')
- 
+
 #' @rdname Portal
 #' @export
 Portal <- component('Portal')
- 
+
 #' @rdname Radio
 #' @export
 Radio <- component('Radio')
- 
+
 #' @rdname RadioGroup
 #' @export
 RadioGroup <- component('RadioGroup')
- 
+
 #' @rdname Rating
 #' @export
 Rating <- component('Rating')
- 
+
 #' @rdname ScopedCssBaseline
 #' @export
 ScopedCssBaseline <- component('ScopedCssBaseline')
- 
+
 #' @rdname Select
 #' @export
 Select <- component('Select')
- 
+
 #' @rdname Skeleton
 #' @export
 Skeleton <- component('Skeleton')
- 
+
 #' @rdname Slide
 #' @export
 Slide <- component('Slide')
- 
+
 #' @rdname Slider
 #' @export
 Slider <- component('Slider')
- 
+
 #' @rdname Snackbar
 #' @export
 Snackbar <- component('Snackbar')
- 
+
 #' @rdname SnackbarContent
 #' @export
 SnackbarContent <- component('SnackbarContent')
- 
+
 #' @rdname SpeedDial
 #' @export
 SpeedDial <- component('SpeedDial')
- 
+
 #' @rdname SpeedDialAction
 #' @export
 SpeedDialAction <- component('SpeedDialAction')
- 
+
 #' @rdname SpeedDialIcon
 #' @export
 SpeedDialIcon <- component('SpeedDialIcon')
- 
+
 #' @rdname Stack
 #' @export
 Stack <- component('Stack')
- 
+
 #' @rdname Step
 #' @export
 Step <- component('Step')
- 
+
 #' @rdname StepButton
 #' @export
 StepButton <- component('StepButton')
- 
+
 #' @rdname StepConnector
 #' @export
 StepConnector <- component('StepConnector')
- 
+
 #' @rdname StepContent
 #' @export
 StepContent <- component('StepContent')
- 
+
 #' @rdname StepIcon
 #' @export
 StepIcon <- component('StepIcon')
- 
+
 #' @rdname StepLabel
 #' @export
 StepLabel <- component('StepLabel')
- 
+
 #' @rdname Stepper
 #' @export
 Stepper <- component('Stepper')
- 
+
 #' @rdname SvgIcon
 #' @export
 SvgIcon <- component('SvgIcon')
- 
+
 #' @rdname SwipeableDrawer
 #' @export
 SwipeableDrawer <- component('SwipeableDrawer')
- 
+
 #' @rdname Switch
 #' @export
 Switch <- component('Switch')
- 
+
 #' @rdname Tab
 #' @export
 Tab <- component('Tab')
- 
+
 #' @rdname TabContext
 #' @export
 TabContext <- component('TabContext')
- 
+
 #' @rdname Table
 #' @export
 Table <- component('Table')
- 
+
 #' @rdname TableBody
 #' @export
 TableBody <- component('TableBody')
- 
+
 #' @rdname TableCell
 #' @export
 TableCell <- component('TableCell')
- 
+
 #' @rdname TableContainer
 #' @export
 TableContainer <- component('TableContainer')
- 
+
 #' @rdname TableFooter
 #' @export
 TableFooter <- component('TableFooter')
- 
+
 #' @rdname TableHead
 #' @export
 TableHead <- component('TableHead')
- 
+
 #' @rdname TablePagination
 #' @export
 TablePagination <- component('TablePagination')
- 
+
 #' @rdname TableRow
 #' @export
 TableRow <- component('TableRow')
- 
+
 #' @rdname TableSortLabel
 #' @export
 TableSortLabel <- component('TableSortLabel')
- 
+
+#' @rdname Tabs
+#' @export
+Tabs <- component('Tabs')
+
 #' @rdname TabList
 #' @export
 TabList <- component('TabList', module = '@mui/lab')
- 
+
 #' @rdname TabPanel
 #' @export
 TabPanel <- component('TabPanel', module = '@mui/lab')
- 
+
 #' @rdname TabContext
 #' @export
 TabContext <- component('TabContext', module = '@mui/lab')
- 
+
 #' @rdname TabScrollButton
 #' @export
 TabScrollButton <- component('TabScrollButton')
- 
+
 #' @rdname TextareaAutosize
 #' @export
 TextareaAutosize <- component('TextareaAutosize')
- 
+
 #' @rdname TextField
 #' @export
 TextField <- component('TextField')
- 
+
 #' @rdname Timeline
 #' @export
 Timeline <- component('Timeline', module = '@mui/lab')
- 
+
 #' @rdname TimelineConnector
 #' @export
 TimelineConnector <- component('TimelineConnector', module = '@mui/lab')
- 
+
 #' @rdname TimelineContent
 #' @export
 TimelineContent <- component('TimelineContent', module = '@mui/lab')
- 
+
 #' @rdname TimelineDot
 #' @export
 TimelineDot <- component('TimelineDot', module = '@mui/lab')
- 
+
 #' @rdname TimelineItem
 #' @export
 TimelineItem <- component('TimelineItem', module = '@mui/lab')
- 
+
 #' @rdname TimelineOppositeContent
 #' @export
 TimelineOppositeContent <- component(
   'TimelineOppositeContent',
   module = '@mui/lab'
 )
- 
+
 #' @rdname TimelineSeparator
 #' @export
 TimelineSeparator <- component('TimelineSeparator', module = '@mui/lab')
- 
+
 #' @rdname ToggleButton
 #' @export
 ToggleButton <- component('ToggleButton')
- 
+
 #' @rdname ToggleButtonGroup
 #' @export
 ToggleButtonGroup <- component('ToggleButtonGroup')
- 
+
 #' @rdname Toolbar
 #' @export
 Toolbar <- component('Toolbar')
- 
+
 #' @rdname Tooltip
 #' @export
 Tooltip <- component('Tooltip')
- 
+
 #' @rdname Typography
 #' @export
 Typography <- component('Typography')
- 
+
 #' @rdname Zoom
 #' @export
 Zoom <- component('Zoom')
