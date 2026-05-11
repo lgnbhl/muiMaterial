7c6bc069012ebb668f54f816edff76a1a521903a
lgnbhl <felix.luginbuhl@protonmail.ch>
2026-05-11 20:39:21 +0200

    fix Autocomplete test and add Autocomplete.shinyInput() test

--- tests/testthat/test-Autocomplete.R
+++ tests/testthat/test-Autocomplete.R
@@ -1,5 +1,37 @@
 test_that("Autocomplete() returns shiny.tag, correct name and value", {
   expect_true(inherits(Autocomplete(), "shiny.tag"))
-  expect_equal(environment(Autocomplete()[["children"]][[2]])[["data"]][["name"]], "Autocomplete")
+  expect_equal(environment(Autocomplete()[["children"]][[2]])[["data"]][["name"]], "AutocompleteStatic")
   expect_equal(environment(Autocomplete("Test")[["children"]][[2]])[["data"]][["props"]][["value"]][["children"]], "Test")
+})
+
+test_that("Autocomplete.shinyInput() returns shiny.tag bound to the Shiny-wired Autocomplete", {
+  tag <- Autocomplete.shinyInput("myId")
+  expect_true(inherits(tag, "shiny.tag"))
+  expect_true(inherits(tag, "muiMaterial"))
+  expect_equal(environment(tag[["children"]][[2]])[["data"]][["name"]], "Autocomplete")
+  expect_equal(environment(tag[["children"]][[2]])[["data"]][["props"]][["value"]][["inputId"]], "myId")
+})
+
+test_that("Autocomplete.shinyInput() validates inputId and inputProps", {
+  expect_error(Autocomplete.shinyInput(123), "inputId")
+  expect_error(
+    Autocomplete.shinyInput("id", inputProps = "not a list"),
+    "`inputProps` must be a list"
+  )
+})
+
+test_that("Autocomplete.shinyInput() warns on conflicting arguments", {
+  child <- shiny::tags$input()
+  expect_warning(
+    Autocomplete.shinyInput("id", child, renderInput = htmlwidgets::JS("function(p){}")),
+    "renderInput.*takes precedence"
+  )
+  expect_warning(
+    Autocomplete.shinyInput("id", child, inputProps = list(label = "x")),
+    "child element takes precedence"
+  )
+  expect_warning(
+    Autocomplete.shinyInput("id", shiny::tags$input(), shiny::tags$input()),
+    "unnamed arguments"
+  )
 })
